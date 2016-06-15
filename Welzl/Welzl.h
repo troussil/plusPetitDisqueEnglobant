70a8d10d4c6589da7eca18896e80cef20b21c99c
Younes.Bosseti <younes.tahri3@gmail.com>
2016-06-15 17:38:55 +0200

    Welzl,erreur de segmentation

---
+++ Welzl/Welzl.h
@@ -0,0 +1,8 @@
+#include "structures.h"
+
+void randomisationTableau(POINT *P, int tailleP);
+int Pointrand (int tailleP);
+CERCLE *b_md(POINT *R,int casesRemplies);
+int defined(CERCLE *C);
+CERCLE *B_MINIDISK(POINT *P,POINT *R,int tailleP,int casesRempliesR);
+CERCLE *MINIDISK(POINT *P,int tailleP);
