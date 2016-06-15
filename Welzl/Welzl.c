a79c045c7da1908aeac0b9213c84350c56a05c10
Younes.Bosseti <younes.tahri3@gmail.com>
2016-06-15 17:35:45 +0200

    welzl,erreur de segmentation

--- Welzl/Welzl.c
+++ Welzl/Welzl.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "structures.h"
 #include "fonctions_resolution_brute.h"
+#include "Welzl.h"
 
 
 //bien se rappeler pour la presentation finale d'expliquer en quoi c'est un algo de complexité lineaire en esperance, et non pas deterministe.(c'est pas evident)
@@ -12,22 +13,22 @@
 void randomisationTableau(POINT *P, int tailleP){
 
 }  
+
 int Pointrand (int tailleP){
   return tailleP-1; //index du point, avec tailleP taille du tableau P
-  //return P[i]; 
+
 }
 
-CERCLE b_md(POINT *R,int casesRemplies){
+CERCLE *b_md(POINT *R,int casesRemplies){
   if(casesRemplies==0){
-    CERCLE C;
-    C.vide=1;
+    CERCLE *C;
     return C;
   }  
   if(casesRemplies==1){
-    CERCLE C;
-    C.d=0;
-    C.x=R[0].x;
-    C.y=R[0].y;
+    CERCLE *C;
+    C->d=0;
+    C->x=R[0].x;
+    C->y=R[0].y;
     return C;
   }   
   if(casesRemplies==2)
@@ -36,17 +37,16 @@ CERCLE b_md(POINT *R,int casesRemplies){
     return cerclePassantParTroisPoints(R[0],R[1],R[2]);
   else{
     printf("error");
-    return 0;
   }
 }    
 
-int defined(CERCLE C){
-
+int defined(CERCLE *C){
+  return 1;
 
 }  
 
-CERCLE B_MINIDISK(POINT *P,POINT *R,int tailleP,int tailleR){
-  CERCLE D;
+CERCLE *B_MINIDISK(POINT *P,POINT *R,int tailleP,int tailleR){
+  CERCLE *D;
   if((tailleP==0) || (tailleR==3))
     D=b_md(R,tailleR);
   else{    
@@ -54,14 +54,15 @@ CERCLE B_MINIDISK(POINT *P,POINT *R,int tailleP,int tailleR){
     POINT point=P[index];
     D= B_MINIDISK(P,R,tailleP-1,tailleR);
     if ((contientPoint(D,point)==0)&&(defined(D)==1)){//si le point n'est pas dans le cercle
+      R[tailleR]=point;
       D=B_MINIDISK(P,R,tailleP-1,tailleR+1);
     }
   }
   return D;    
 }
 
-CERCLE MINIDISK(POINT *P,int tailleP){
+CERCLE *MINIDISK(POINT *P,int tailleP){
   POINT *R;
   randomisationTableau(P,tailleP);
-  return B_MINIDISK(P,R,tailleP,tailleR);
+  return B_MINIDISK(P,R,tailleP,0);
 }  
