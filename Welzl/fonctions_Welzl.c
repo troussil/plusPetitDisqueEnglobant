ef75d709af80c2721e2a0874b230dde0bebc64f2
Younes.Bosseti <younes.tahri3@gmail.com>
2016-06-20 18:18:24 +0200

    Erreur de segmentation

--- Welzl/fonctions_Welzl.c
+++ Welzl/fonctions_Welzl.c
@@ -3,6 +3,7 @@
 #include "structures.h"
 #include "fonctions_resolution_brute.h"
 #include "fonctions_Welzl.h"
+#include <time.h>
 
 
 //bien se rappeler pour la presentation finale d'expliquer en quoi c'est un algo de complexité lineaire en esperance, et non pas deterministe.(c'est pas evident)
@@ -11,7 +12,16 @@
 
 
 void randomisationTableau(POINT *P, int tailleP){
-
+    /*int i=0;
+    int nombre_tire=0;
+    int temp=0;
+    for(i = 0; i< tailleP;i++){
+        nombre_tire=rand()%tailleP;
+        // On échange les contenus des cases i et nombre_tire
+        temp = P[i];
+        P[i] = P[nombre_tire];
+        P[nombre_tire]=temp;
+    }*/
 }  
 
 int Pointrand (int tailleP){
@@ -40,8 +50,9 @@ CERCLE *b_md(POINT *R,int casesRemplies){
   }
 }    
 
-int defined(CERCLE *C){
-  return 1;
+int defined(CERCLE *C,POINT *P,int taille){
+  int ok=contientTousPoint(C,P,taille);
+  return ok;
 
 }  
 
@@ -53,7 +64,7 @@ CERCLE *B_MINIDISK(POINT *P,POINT *R,int tailleP,int tailleR){
     int index=Pointrand(tailleP);
     POINT point=P[index];
     D= B_MINIDISK(P,R,tailleP-1,tailleR);
-    if ((contientPoint(D,point)==0)&&(defined(D)==1)){//si le point n'est pas dans le cercle
+    if ((contientPoint(D,point)==0)&&(defined(D,P,tailleP-1)==1)){//si le point n'est pas dans le cercle
       R[tailleR]=point;
       D=B_MINIDISK(P,R,tailleP-1,tailleR+1);
     }
--- Welzl/fonctions_Welzl.h
+++ Welzl/fonctions_Welzl.h
@@ -3,6 +3,6 @@
 void randomisationTableau(POINT *P, int tailleP);
 int Pointrand (int tailleP);
 CERCLE *b_md(POINT *R,int casesRemplies);
-int defined(CERCLE *C);
+int defined(CERCLE *C,POINT *P,int taille);
 CERCLE *B_MINIDISK(POINT *P,POINT *R,int tailleP,int casesRempliesR);
 CERCLE *MINIDISK(POINT *P,int tailleP);
